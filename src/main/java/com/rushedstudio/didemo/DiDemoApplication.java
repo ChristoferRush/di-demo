1c05223bf31528350c929f94ee499ce3d3183519
Krzysztof Pośpiech <christoferrush@gmail.com>
2018-05-04 14:19:34 +0200

    --

--- src/main/java/com/rushedstudio/didemo/DiDemoApplication.java
+++ src/main/java/com/rushedstudio/didemo/DiDemoApplication.java
@@ -7,8 +7,11 @@ import com.rushedstudio.didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan("com.rushedstudio.didemo")
 public class DiDemoApplication {
 
     public static void main(String[] args) {
