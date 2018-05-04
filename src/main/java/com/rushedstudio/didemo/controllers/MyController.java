0975b77546f6481db28759b096be580260f78b35
Krzysztof Pośpiech <christoferrush@gmail.com>
2018-05-04 10:52:21 +0200

    primary annotation

--- src/main/java/com/rushedstudio/didemo/controllers/MyController.java
+++ src/main/java/com/rushedstudio/didemo/controllers/MyController.java
@@ -1,12 +1,19 @@
 package com.rushedstudio.didemo.controllers;
 
+import com.rushedstudio.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello(){
         System.out.println("Hello in console");
-        return "hello";
+        return greetingService.sayGreeting();
     }
 }
